bae7fc0cdac1631cb6211bb406c90631d3343364
Hack-Jay <735059024@qq.com>
2019-02-24 18:34:58 +0800

    initial

---
+++ build/webpack.dev.config.js
@@ -0,0 +1,43 @@
+const path = require("path")
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const HtmlPlugin = require('html-webpack-plugin')
+const baseConfig = require('./webpack.base.config')
+
+const isDev = process.env.NODE_ENV === 'development'
+const config = merge(baseConfig, {
+	mode: 'development',
+  entry: path.join(__dirname, '../src', 'index'),
+	plugins: [
+		new HtmlPlugin({
+			template: path.join(__dirname, '../src/index.html'),
+			filename: path.join(__dirname, './index.html')
+		})
+	]
+})
+
+if(isDev) {
+	config.entry = {
+		app: [
+			'react-hot-loader/patch',
+			path.join(__dirname, '../src/index.js')
+		]
+	},
+	config.devServer = {
+    contentBase: path.join('../dist/'),
+    inline: true,
+    host: '0.0.0.0',
+		port: 8080,
+		overlay: {
+			errors: true
+		},
+		hot: true,
+		publicPath:'/dist/',
+		historyApiFallback: {
+			index: '/dist/index.html'
+    },
+	}
+	config.plugins.push(new webpack.HotModuleReplacementPlugin())
+}
+
+module.exports = config
