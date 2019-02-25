77fe6675345434ed4e0317c7be315c94cf986e07
Hack-Jay <735059024@qq.com>
2019-02-25 22:36:35 +0800

    header commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './index.less';
+
+class Header extends React.PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = {
+			list: [
+				{
+					title: '首页'
+				},
+				{
+					title: '动态'
+				},
+				{
+					title: '话题'
+				},
+				{
+					title: '小册'
+				},
+				{
+					title: '活动'
+				}
+			]
+		};
+	}
+	render() {
+		return (
+			<div className="main-header">
+				<div className="container">
+					<a className="logo">
+            <img src="https://b-gold-cdn.xitu.io/v3/static/img/logo.a7995ad.svg" alt="logo" />
+          </a>
+					<ul className="nav-list">
+						{this.state.list.map((item) => (
+							<li>
+								<a>{item.title}</a>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Header;
--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Header from '../../components/Header';
 
 class Home extends React.Component {
 	render() {
 		return (
-			<div>header</div>
-		)
+			<div>
+				<Header />
+			</div>
+		);
 	}
 }
 
-export default Home
+export default Home;
